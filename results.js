e27bdb007d585dbaf8068a1cb75621770779cea9
Kimberly Suares <kimberlyjsuares@gmai.com>
2023-11-11 21:04:37 -0500

    updated shop cards

--- results.js
+++ results.js
@@ -1,47 +1,32 @@
-// Function to navigate to the next step of the quiz
-function nextStep(event) {
-	event.preventDefault(); // Prevents the default button behavior
+const startBtn = document.querySelector("#heroBtn");
+const hero = document.querySelector(".hero-section ");
+const form = document.querySelector(".quiz-container");
 
-	const currentStep = event.target.closest('.quiz-step');
-	const nextStep = currentStep.nextElementSibling;
+startBtn.addEventListener("click", () => {
+  hero.classList.remove("show");
+  form.classList.add("show");
+});
 
-	if (nextStep) {
-		currentStep.classList.remove('show');
-		nextStep.classList.add('show');
-	}
+//  Start Quiz
+let currentStep = 1;
+
+function nextStep(e) {
+  e.preventDefault();
+  const currentStepElement = document.getElementById(`step${currentStep}`);
+  currentStepElement.style.display = "none";
+
+  currentStep++;
+  const nextStepElement = document.getElementById(`step${currentStep}`);
+  if (nextStepElement) {
+    nextStepElement.style.display = "block";
+  } else {
+    alert("End of quiz");
+  }
 }
 
-// Function to handle the final form submission
-function submitQuiz(event) {
-	event.preventDefault(); // Prevents the default form submission behavior
+function submitQuiz(e) {
+  e.preventDefault();
+  //  Something happens
 
-	// Get selected values from the form
-	const dressCode = getSelectedValue('dressCode');
-	const preferredBrand = getSelectedValue('preferredBrand');
-	const workplaceClimate = getSelectedValue('workplaceClimate');
-	const itemsLookingFor = getSelectedValue('itemsLookingFor');
-	const budgetRange = getSelectedValue('budgetRange');
-
-	// Determine the route based on quiz answers
-	let route = '';
-	if (dressCode === 'option1') {
-		route = 'shop.html#first-card';
-	} else if (dressCode === 'option2') {
-		route = 'shop.html#second-card';
-	}
-	// Add more conditions for other possible routes based on quiz answers
-
-	// Redirect to the determined route
-	window.location.href = route;
-}
-
-// Function to get the selected value from a group of radio buttons
-function getSelectedValue(name) {
-	const radioButtons = document.getElementsByName(name);
-	for (const radioButton of radioButtons) {
-		if (radioButton.checked) {
-			return radioButton.value;
-		}
-	}
-	return null; // Return null if no option is selected
+  window.location.href = "shop.html";
 }
